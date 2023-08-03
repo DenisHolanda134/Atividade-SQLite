cdfcc413598a6d7a4b7d271c25b7e2a456fc558f
Denis Holanda <134006611+DenisHolanda134@users.noreply.github.com>
2023-08-03 16:24:45 -0300

    Add files via upload

---
+++ Entrega_de_Eclipse_e_SQLite/_ProjetoJava/Biblioteca/src/br/com/biblioteca/Usuario.java
@@ -0,0 +1,16 @@
+package br.com.biblioteca;
+
+import java.sql.*;
+
+public class Usuario {
+
+}
+public String nome, telefone, rua, numero, complemento, cep, bairro, cidade, estado;
+public int codUsuario;
+public String URL = "jdbc:sqlite:banco.db";
+public Connection conecction;
+
+public Usuario{
+	
+	
+}
